7d9fff1e292c5689b486dbdce37a221bdd71e2c1
emerickCHALET <47389946+emerickCHALET@users.noreply.github.com>
2023-01-02 18:59:33 +0100

    modification error 404 page

--- newboard/src/pages/Error.tsx
+++ newboard/src/pages/Error.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import SideBar from "../components/SideBar";
+import Footer from "../components/Footer";
 
 const ErrorPage = () => {
     return (
-        <h1> Hehehe Roor 404</h1>
-    )
+        <div className="wrap">
+            <SideBar/>
+            <img
+                alt=""
+                src="/404.png"
+                width="700"
+                height="400"
+                className="d-inline-block align-top center"
+
+            />
+        <Footer/>
+        </div>
+)
 }
 
 export default ErrorPage;
